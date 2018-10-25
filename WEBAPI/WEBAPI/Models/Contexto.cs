a0a7fd06ab41510d2f6907687b9ea474b2e18b71
AlbertoLuisM <alberto.luis.m@hotmail.com>
2018-10-25 20:40:46 -0300

    Initial commit

---
+++ WEBAPI/WEBAPI/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace WEBAPI.Models
+{
+    public class Contexto:DbContext
+    {
+        public Contexto() : base("APIADS") { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        //implementar demais classes
+    }
+}
---
+++ WEBAPI/WEBAPI/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace WEBAPI.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [StringLength(35)]
+        public string descricao { get; set; }
+
+        public float quantidade { get; set; }
+        public float valor { get; set; }
+
+
+    }
+}
